3f7e0430c9333babcd4743077966b972ea7da752
yuhangjiang22 <74789310+yuhangjiang22@users.noreply.github.com>
2025-04-07 16:33:44 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,10 @@ import importlib
 import re
 import pickle
 import json
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.asymmetric import padding as rsa_padding
 
 # %% body
 def recursive_lowercase(data):
@@ -78,3 +82,78 @@ def make_dir(dir_path):
         print(f"Directory created at: {dir_path}")
     else:
         print(f"Directory already exists at: {dir_path}")
+
+def pickle_save_encrypted(data, filename, public_key_path):
+    # Serialize dataset (can be large)
+    serialized_data = pickle.dumps(data)
+
+    # Generate random AES key and IV
+    aes_key = os.urandom(32)  # AES-256
+    iv = os.urandom(16)
+
+    # Encrypt the serialized data with AES
+    cipher = Cipher(algorithms.AES(aes_key), modes.CBC(iv))
+    encryptor = cipher.encryptor()
+
+    # PKCS7 padding
+    padding_len = 16 - (len(serialized_data) % 16)
+    padded_data = serialized_data + bytes([padding_len] * padding_len)
+
+    encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
+
+    # Load RSA public key
+    with open(public_key_path, "rb") as key_file:
+        public_key = serialization.load_pem_public_key(key_file.read())
+
+    # Encrypt AES key with RSA
+    encrypted_aes_key = public_key.encrypt(
+        aes_key,
+        rsa_padding.OAEP(
+            mgf=rsa_padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    # Save to file: [2-byte key length][encrypted AES key][IV][encrypted data]
+    with open(filename, "wb") as f:
+        f.write(len(encrypted_aes_key).to_bytes(2, "big"))
+        f.write(encrypted_aes_key)
+        f.write(iv)
+        f.write(encrypted_data)
+
+def pickle_load_encrypted(filename, private_key_path, password=None):
+
+    # Load private RSA key
+    with open(private_key_path, "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=password.encode() if password else None,
+        )
+
+    with open(filename, "rb") as f:
+        key_len = int.from_bytes(f.read(2), "big")
+        encrypted_key = f.read(key_len)
+        iv = f.read(16)
+        encrypted_data = f.read()
+
+    # Decrypt AES key
+    aes_key = private_key.decrypt(
+        encrypted_key,
+        rsa_padding.OAEP(
+            mgf=rsa_padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    # Decrypt data
+    cipher = Cipher(algorithms.AES(aes_key), modes.CBC(iv))
+    decryptor = cipher.decryptor()
+    padded_data = decryptor.update(encrypted_data) + decryptor.finalize()
+
+    # Remove PKCS7 padding
+    padding_len = padded_data[-1]
+    data = padded_data[:-padding_len]
+
+    return pickle.loads(data)
